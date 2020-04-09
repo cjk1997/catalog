4ddf899e80371aa63d5cd6b049e80ed993634814
Carson Keller <carsonjkeller@gmail.com>
2020-04-09 16:54:35 -0600

    Added other defined functions; might remove later

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+const { 
+  getArticles,
+  addArticle,
+  updateArticle,
+  deleteArticle
+} = require('../data/articles');
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Catalog' });
+});
+
+router.get('/articles', async function(req, res, next) {
+  try {
+    const data = await getArticles();
+    res.render('articles', { title: "Articles", data: data });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Internal Server Issue, check log');
+  };
 });
 
 module.exports = router;
