894b143bf1b57e95d10e406300027e5a48e35d4c
Carson Keller <carsonjkeller@gmail.com>
2020-04-09 16:56:44 -0600

    Created and implemented submit/delete handlers

---
+++ public/javascripts/articles.js
@@ -0,0 +1,41 @@
+const title = document.getElementById('Title');
+const author = document.getElementById('Author');
+const link = document.getElementById('Link');
+const description = document.getElementById('Description');
+const submit = document.getElementById('Submit');
+
+const deleteButtons = document.getElementsByClassName('delete');
+
+
+const submitHandler = () => {
+    console.log('Attempting to submit...')
+    let data = [{
+        'title': title.value,
+        'author': author.value,
+        'link': link.value,
+        'description': description.value
+    }];
+    fetch(`api/articles`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(() => window.location.replace('/articles'));
+};
+
+submit.addEventListener('click', submitHandler);
+
+
+const deleteHandler = () => {
+    console.log('ID: ', event.target.id);
+    fetch(`/api/articles/${event.target.id}`, {
+        method: 'delete'
+    }).then(() => window.location.replace('/articles'));
+};
+
+for (let i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].addEventListener('click', deleteHandler);
+};
+
+submit.addEventListener('click', deleteHandler);
